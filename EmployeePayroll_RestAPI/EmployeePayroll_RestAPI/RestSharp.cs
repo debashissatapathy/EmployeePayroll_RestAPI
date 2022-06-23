26606d04bf166fc810b7d84c6088a57830848399
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-06-23 15:33:41 +0530

    Retrieve Employee detail and MS test

---
+++ EmployeePayroll_RestAPI/EmployeePayroll_RestAPI/RestSharp.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayroll_RestAPI
+{
+    public class RestSharp
+    {
+        public void givenEmployee_OnPost_ShouldReturnAddedEmployee()
+        {
+            //arrange
+            RestRequest request = new RestRequest("/Employee", Method.Post);
+            JObject jobjectbody = new JObject();
+            jobjectbody.Add("Name", "Niketan");
+            jobjectbody.Add("Salary", "35000");
+
+            request.AddParameter("application/json", jobjectbody, ParameterType.RequestBody);
+
+        }
+    }
+}
